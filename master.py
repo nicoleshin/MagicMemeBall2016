0f5679b6b073b5b8a05fd66173bb6d24f638f099
Helen Ye <00yhelen@gmail.com>
2016-04-16 21:02:27 -0400

    Add HTML output to python

--- master.py
+++ master.py
@@ -74,5 +74,55 @@ def image(string):#():
             failsafe.append(correctImg(choose(firstWords)) + ' ')
         return choose(failsafe)
 
+def htmlPrint(img):
+    print '''<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
+        <title>Magic Meme Ball</title>
+        <link href="materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+         <style>
+            body {
+                width: 100%;
+                height: 100%;
+                color: white;
+                background-color: black;
+            }
+            #text {
+                padding-left: 200px;
+                padding-right: 200px;
+            }
+            ::-webkit-input-placeholder { /* WebKit, Blink, Edge */
+                color: gray;
+            }
+            :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+                color: gray;
+                opacity:  1;
+            }
+            ::-moz-placeholder { /* Mozilla Firefox 19+ */
+                color: gray;
+                opacity:  1;
+            }
+            :-ms-input-placeholder { /* Internet Explorer 10-11*/
+                color: gray;
+            }
+        </style>
+    </head>
+    <body>
+        <img src=''' + img + '''
+        <div class="row center">
+            <a href="question.html"><button class="btn waves-effect waves-light"
+                    id="button"  type="button">More answers...</button></a>
+        or
+            <a href="advice.html"><button class="btn waves-effect waves-light"
+                    id="button" type="button">More advice...</button></a>
+        </div>
+    </body>
+</html>
+'''
+
 print image('what ')
 print image('fuqery ')
